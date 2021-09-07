adf5052e603fb87899491c4736f4ec12266c8d19
Ankit222002 <aj9818013263@gmail.com>
2021-09-07 20:46:17 +0530

    https://github.com/Ankit222002/square-turtle.py.git

---
+++ square turtle.py
@@ -0,0 +1,31 @@
+import turtle
+
+qazi_turtle = turtle.Turtle()
+qazi_turtle.speed(4)
+
+
+def square():
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.left(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.left(100)
+    qazi_turtle.right(90)
+    qazi_turtle.left(90)
+
+
+
+for count in range (20):
+    square()
+
+
+
+
+
